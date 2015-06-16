718654ff6b6092aed190bcb6438ecbed5afb0b79
Douglas, Nathan [CTR] <ndouglas@sphereoi.com>
2015-06-15 21:34:52 -0400

    Updated database to Postgres for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150615150347) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "emails", force: :cascade do |t|
     t.text     "body"
     t.integer  "user_id"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20150615150347) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "emails", ["user_id"], name: "index_emails_on_user_id"
+  add_index "emails", ["user_id"], name: "index_emails_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
@@ -34,4 +37,5 @@ ActiveRecord::Schema.define(version: 20150615150347) do
     t.string   "emailaddress"
   end
 
+  add_foreign_key "emails", "users"
 end
