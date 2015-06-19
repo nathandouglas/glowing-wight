9f292d3e5c319e8664614513237d8146eb89c823
Douglas, Nathan [CTR] <ndouglas@sphereoi.com>
2015-06-19 00:08:14 -0400

    Updated homepage, removed turbolink from header on root path

--- app/assets/javascripts/welcome.coffee
+++ app/assets/javascripts/welcome.coffee
@@ -1,8 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
- 
-#$(document).ready -> 
-#  $('.parallax').parallax();
+
 $(document).ready ->
   $('.slider').slider({full_width: true})
+  $('.parallax').parallax()
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  # Must be set in order to allow google image proxy for gmail
   config.action_mailer.asset_host = 'https://aqueous-crag-5152.herokuapp.com/'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
