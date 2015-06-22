f32107b045cd815e74ca0e928fa6df796c1a52e5
Douglas, Nathan [CTR] <ndouglas@sphereoi.com>
2015-06-22 15:01:50 -0400

    popping stash

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,9 @@ class UserMailer < ApplicationMailer
   def thanks_email(user)
     @user = user
     @url = 'http://example.com/Login'
-    mail(to: @user.emailaddress, subject: 'Thank you for interest in Kevin & Sons Lawncare')
+
+    if !user.emailaddress.empty?
+      mail(to: @user.emailaddress, subject: 'Thank you for interest in Kevin & Sons Lawncare')
+    end
   end
 end
